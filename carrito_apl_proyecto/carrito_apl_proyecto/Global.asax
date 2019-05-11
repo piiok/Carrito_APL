fc52cdc08122a3abeb8cc63e0edb14e18fc15d04
piiok <33143055+piiok@users.noreply.github.com>
2019-05-11 08:32:12 -0500

    proyecto renovado

---
+++ carrito_apl_proyecto/carrito_apl_proyecto/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="carrito_apl_proyecto.MvcApplication" Language="C#" %>
---
+++ carrito_apl_proyecto/carrito_apl_proyecto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(carrito_apl_proyecto.Startup))]
+namespace carrito_apl_proyecto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
